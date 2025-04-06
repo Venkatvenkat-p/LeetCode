c25042104dee078a3a4045626557eb0f08b1e8ff
Venkatraman P <pvenkat5678@gmail.com>
2025-04-06 21:42:52 +0530

    Time: 0 ms (100%), Space: 44.5 MB (89.38%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int []ans=new int[nums.length];
+        int index=0;
+        for(int i=0;i<nums.length/2;i++){
+            ans[index++]=nums[i];
+            ans[index++]=nums[nums.length/2+i];
+        }
+        return ans;
+        
+    }
+}
