54fd9fe8b0559997629b86484d4598d320dbfe1c
Venkatraman P <pvenkat5678@gmail.com>
2025-04-21 11:14:56 +0530

    Time: 0 ms (100%), Space: 40.7 MB (93.26%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,6 @@
 class Solution {
     public int mySqrt(int x) {
-        double sqroot = Math.sqrt(x);
-        int num = (int)sqroot;
-        return num;
+       return (int) Math.sqrt(x)
+       ;
     }
 }
