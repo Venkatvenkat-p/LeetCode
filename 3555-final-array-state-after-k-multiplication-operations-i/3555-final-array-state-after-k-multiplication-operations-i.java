a61d77d2323011eb2544a139deb48ede7aae0002
Venkatraman P <pvenkat5678@gmail.com>
2025-05-11 20:41:47 +0530

    Time: 1 ms (100%), Space: 44.8 MB (87.07%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,23 +1,18 @@
 class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-          int n=nums.length;
-        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
-
-        for(int i=0; i<n; i++){
-            pq.add(new int[]{nums[i],i});
+                for (; k>0; k--) {
+            var min = Integer.MAX_VALUE;
+            for (var i=0; i<nums.length; i++) {
+                min = Math.min(min, nums[i]);
+            }
+            for (var i=0; i<nums.length; i++) {
+                if (nums[i] == min) {
+                    nums[i] *= multiplier;
+                    break;
+                }
+            }
         }
-
-       
-        for(int i=0; i<k; i++){
-            int[] arr=pq.poll();
-            int val=arr[0];
-            int idx=arr[1];
-
-            nums[idx]=val*multiplier;
-            pq.add(new int[]{nums[idx],idx});
-        }
-
-        
         return nums;
+
     }
 }
