acf2377f04ecc86b4113003400265c010aa3cbf4
Venkatraman P <pvenkat5678@gmail.com>
2026-02-06 09:38:30 +0530

    Time: 0 ms (100%), Space: 41.9 MB (99.96%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,15 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        return(arr[arr.length-1].length());
-
+        int count = 0;
+        s = s.trim();
+        for(int i=s.length() -1;i>=0;i--){
+            if(s.charAt(i) == ' '){
+                break;
+            }
+            else{
+                count++;
+            }
+        }
+        return count;
     }
 }
